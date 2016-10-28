f9dd64c9f24ed1551689b58c01bf355af6d52ad6
eino <838578941@qq.com>
2016-10-28 14:38:26 +0800

    first init

---
+++ LuceneProject/src/QuickStart/Index.java
@@ -0,0 +1,36 @@
+package QuickStart;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+public class Index {
+	public static void main(String args[]) throws IOException{
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		Directory index = new RAMDirectory();
+		
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		
+		IndexWriter w = new IndexWriter(index, config);
+		addDoc(w, "Lucene in Action", "193398817");
+		addDoc(w, "Lucene for Dummies", "55320055Z");
+		addDoc(w, "Managing Gigabytes", "55063554A");
+		w.close();
+		
+	}
+	
+	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException{
+		Document doc = new Document();
+		doc.add(new TextField("title", title, Field.Store.YES));
+		doc.add(new StringField("isbn", isbn, Field.Store.YES));
+		w.addDocument(doc);
+	}
+}
